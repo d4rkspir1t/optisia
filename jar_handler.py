7845d5f669d1fa226abdf0ad89d8443d9ad71222
Unknown <svmotric@gmail.com>
2019-03-14 10:27:08 +0100

    .gitignore for handling pycharm .idea folder omnisia.jar jar caller script

---
+++ jar_handler.py
@@ -0,0 +1,25 @@
+import subprocess
+import numpy
+import logging
+
+
+class JarCall:
+    def __init__(self, path):
+        self.base_call = ['java', '-jar']
+        self.jar_path = path
+
+    def make_call(self, params=[]):
+        call = self.base_call
+        call.append(self.jar_path)
+        call.extend(params)
+
+        print('CALL:\t', call)
+        print('-'*50, '\n')
+
+        output = subprocess.call(call, stdout=None, stderr=None)
+        if output > 0:
+            print('Warning, result was %s ' % output)
+
+
+# caller = JarCall(path='omnisia.jar')
+# caller.make_call(['--help'])
